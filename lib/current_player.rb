5b4c8269f681f98c4c80a21738904bc0ac372f6b
Elana Ryznar <eryzna@gmail.com>
2017-11-05 23:50:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,21 +1,9 @@
 def turn_count(board)
-  turns = 0
+  counter = 0
   board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
   board.each do |turn|
     if token == "X" || token == "O"
       turns += 1
     end
   end
-end
-
-def turn_count(board)
-
-  turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      turns += 1
-    end
-  end
-  turns
-
 end
